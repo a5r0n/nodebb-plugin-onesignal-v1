bde0c90c58038e20f9502990dec353ae0b51249c
אהרן הורן <h98aaron@gmail.com>
2019-08-16 14:52:32 +0300

    OneSignal #11

--- static/lib/ClientSide.js
+++ static/lib/ClientSide.js
@@ -3,37 +3,42 @@
 /* globals document, $ */
 
 $(document).ready(function() {
-  var OneSignal = window.OneSignal || [];
-  OneSignal.push(function() {
-    OneSignal.on("subscriptionChange", function(isSubscribed) {
-      console.log("The user's subscription state is now:", isSubscribed);
-      OneSignal.push(function() {
-        OneSignal.getUserId(function(userId) {
-          console.log("OneSignal User ID:", userId);
-          fetch("/api/me/onesignal/devices", {
-            method: "POST",
-            body: JSON.stringify({
-              player_id: userId
+  $.getScript("https://cdn.onesignal.com/sdks/OneSignalSDK.js", function(
+    data,
+    textStatus,
+    jqxhr
+  ) {
+    var OneSignal = window.OneSignal || [];
+    OneSignal.push(function() {
+      OneSignal.on("subscriptionChange", function(isSubscribed) {
+        console.log("The user's subscription state is now:", isSubscribed);
+        OneSignal.push(function() {
+          OneSignal.getUserId(function(userId) {
+            console.log("OneSignal User ID:", userId);
+            fetch("/api/me/onesignal/devices", {
+              method: "POST",
+              body: JSON.stringify({
+                player_id: userId
+              })
             })
-          })
-            .then(function(response) {
-              return response.json();
-            })
-            .then(function(myJson) {
-              console.log(JSON.stringify(myJson));
-            });
+              .then(function(response) {
+                return response.json();
+              })
+              .then(function(myJson) {
+                console.log(JSON.stringify(myJson));
+              });
+          });
         });
       });
     });
-  });
-  OneSignal.push(function() {
-    OneSignal.init({
-      appId: "5dcb3af0-7a2b-4051-880e-22c28adceea5",
-      path: "/plugins/nodebb-plugin-onesignal/static/lib/",
-      autoResubscribe: true
+    OneSignal.push(function() {
+      OneSignal.init({
+        appId: "5dcb3af0-7a2b-4051-880e-22c28adceea5",
+        path: "/plugins/nodebb-plugin-onesignal/static/lib/",
+        autoResubscribe: true
+      });
     });
   });
-  $.getScript("https://cdn.onesignal.com/sdks/OneSignalSDK.js");
   console.log("nodebb-plugin-onesignal: loaded");
   // Note how this is shown in the console on the first load of every page
 });
