fe3d6c54f5babe2f685a042a8784d3109d9401e2
אהרן הורן <h98aaron@gmail.com>
2019-08-18 03:26:11 +0300

    OneSignal #13

--- static/lib/ClientSide.js
+++ static/lib/ClientSide.js
@@ -10,16 +10,30 @@ $(document).ready(function() {
   ) {
     var OneSignal = window.OneSignal || [];
     OneSignal.push(function() {
-      OneSignal.on("subscriptionChange", function(isSubscribed) {
-        console.log("The user's subscription state is now:", isSubscribed);
-        window.OneSignal.getUserId(function(userId) {
-          console.log("OneSignal User ID:", userId);
-          fetch("/api/me/onesignal/devices", {
+      OneSignal.push([
+        "init",
+        {
+          appId: "5dcb3af0-7a2b-4051-880e-22c28adceea5",
+          notifyButton: {
+            enable: false,
+            position: "bottom-right",
+            offset: {
+              bottom: "50px"
+            }
+          },
+          autoResubscribe: true
+        }
+      ]);
+
+      function onSubscribeButtonClicked() {
+        OneSignal.getUserId(function(userId) {
+          const options = {
             method: "POST",
             body: JSON.stringify({
               player_id: userId
             })
-          })
+          };
+          fetch("/api/me/onesignal/devices", options)
             .then(function(response) {
               return response.json();
             })
@@ -27,13 +41,16 @@ $(document).ready(function() {
               console.log(JSON.stringify(myJson));
             });
         });
-      });
-    });
-    OneSignal.push(function() {
-      OneSignal.init({
-        appId: "5dcb3af0-7a2b-4051-880e-22c28adceea5",
-        path: "/plugins/nodebb-plugin-onesignal/static/lib/",
-        autoResubscribe: true
+      }
+
+      function onUnsubscribeButtonClicked() {}
+
+      OneSignal.on("subscriptionChange", function(isSubscribed) {
+        if (isSubscribed) {
+          onSubscribeButtonClicked();
+        } else {
+          onUnsubscribeButtonClicked();
+        }
       });
     });
   });
